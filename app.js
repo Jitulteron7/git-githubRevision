e85dbce4ea96abe6ffda1dcf55192bda5bbd6516
Your Name <you@example.com>
2020-09-25 13:18:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function name(){
+    console.log("teron");
+}
+// hit add <filename> to add to the staging area
+// git commit to make a check point 
+// check point is a point in the timeline in which the file can be converted (if needed) to that time line of the commit
+// we name the first commit as initial commit
