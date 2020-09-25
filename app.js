bddfc2838ac3bc1348ddc0cd7ff1dc0b9934e999
Your Name <you@example.com>
2020-09-25 14:31:38 +0530

    commit dev 4

--- app.js
+++ app.js
@@ -11,3 +11,5 @@ function name(){
 // git branch <name of the branch> to make a new branch
 //  git checkout <name of the branch> to go to the named branch
 // git commit -am <filename> it is used to add and commit at the same time
+// now to merge a branch to another use git branch merge <Branch_Name>
+// working with from local repo to remote repo us git 
